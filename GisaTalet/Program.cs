0152099e1c531968914f48ec04b07f849cc19784
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2025-09-22 21:32:09 +0200

    GisaTalet game

--- GisaTalet/Program.cs
+++ GisaTalet/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace GisaTalet
+﻿using System.Diagnostics;
+
+namespace GisaTalet
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+                Random random = new Random();
+                bool spelaIgen = true;
+
+               while (spelaIgen)
+                {
+                int hemligTal= random.Next(1, 101);
+                int antalGissningar = 0;
+                int gissning = 0;
+
+                Console.WriteLine("Välkommen till Gissa Talet!");
+                Console.WriteLine("Jag har valt ett tal mellan 1 och 100. Kan du gissa vilket?");
+               
+               
+            // Här börjar själva gissningsloopen
+            while (gissning != hemligTal)
+                {
+                    Console.Write("Ange din gissning: ");
+                    bool correctInput = int.TryParse(Console.ReadLine(), out gissning);
+                    if (!correctInput)
+                    {
+                        Console.WriteLine("Var god ange ett giltigt nummer.");
+                        continue;
+                    }
+                    antalGissningar++;
+                    if (gissning < hemligTal)
+                    {
+                        Console.WriteLine("För lågt! Försök igen.");
+                    }
+                    else if (gissning > hemligTal)
+                    {
+                        Console.WriteLine("För högt! Försök igen.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Grattis! Du gissade rätt på {antalGissningar} försök.");
+                    }
+
+                    // Ge en ledtråd efter 5 felaktiga gissningar
+                    if (antalGissningar == 5 && gissning != hemligTal)
+                    {
+                        if (hemligTal % 2 == 0)
+                        {
+                            Console.WriteLine("Ledtråd: Talet är jämnt.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ledtråd: Talet är udda.");
+                        }
+                    }
+
+
+
+                }
+            }
 }
